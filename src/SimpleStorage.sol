e22e9a4292a3316581f563ff29dacec122ad844c
LordDregg <oluwatosinodejide121@gmail.com>
2025-10-27 14:00:34 +0100

    Written SmartCoontract

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -2,5 +2,20 @@
 pragma solidity ^0.8.19;
 
 contract SimpleStorage {
-  
+  struct Person {
+    uint256 favoriteNumber;
+    string name;
+  }
+
+  Person[] public sListOfPeople;
+
+  // Setters
+  function addPersonToListOfPeopleArray(uint256 favoriteNumber, string memory name) public {
+    sListOfPeople.push(Person({
+      favoriteNumber: favoriteNumber,
+      name: name
+    }));
+  }
+
+  // Getters
 }
